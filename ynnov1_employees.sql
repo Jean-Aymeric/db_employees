940d01cb9e0bb8d5d5b56609a861178e9553059d
Jean-Aymeric Diet <jeanaymeric@gmail.com>
2024-12-20 10:21:53 +0100

    Initial commit

---
+++ ynnov1_employees.sql
@@ -0,0 +1,17 @@
+CREATE VIEW ynnov1_employees AS
+SELECT employees.emp_no,
+       employees.birth_date,
+       employees.first_name,
+       employees.last_name,
+       employees.gender,
+       employees.hire_date,
+       salaries.salary,
+       titles.title,
+       dept_emp.dept_no
+FROM employees
+INNER JOIN salaries ON employees.emp_no = salaries.emp_no
+INNER JOIN titles on employees.emp_no = titles.emp_no
+INNER JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
+WHERE salaries.to_date = '9999-01-01'
+AND titles.to_date = '9999-01-01'
+AND dept_emp.to_date = '9999-01-01';
